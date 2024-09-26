beb9166c78329e6951ad1e00ee72ac756ba0e5a2
Dawson-Bruce <brucedj@mail.uc.edu>
2024-09-26 02:01:07 -0400

    Completed Task 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        // Declare variables
+        Scanner in = new Scanner(System.in);
+        int age = 0;
+
+        // Prompt user for age
+        System.out.print("Please enter your age: ");
+
+        // Check if input is an integer
+        if (in.hasNextInt())
+        {
+            age = in.nextInt();
+
+            // Check if age >= 21
+            if (age >= 21)
+            {
+                System.out.println("You get a wristband.");
+            }
+        } else {
+            System.out.println("Invalid age: " + in.nextLine());
+        }
     }
 }
